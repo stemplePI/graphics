e944ab53ae54b556ea0e4fabeba916648fce6845
YSSD\stemple <stemple@yssd.org>
2022-09-01 09:11:08 -0400

    add a jpanel to project

---
+++ ThursdayGUI/src/myFirstGraphics/box.java
@@ -0,0 +1,16 @@
+package myFirstGraphics;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class box extends JPanel{
+
+	public box(Color c) {
+		super.setBackground(c);
+		
+	}
+	
+	
+	
+	
+}//end class
